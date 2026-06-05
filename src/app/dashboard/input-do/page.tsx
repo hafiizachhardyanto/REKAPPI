15f38d3bfed6529a2cad76f1718ac93a8bf5c720
hafiizachhardyanto <hafiizachmadhardyanto@gmail.com>
2026-06-05 20:29:05 +0700

    fix PI

---
+++ src/app/dashboard/input-do/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import Header from "@/app/components/ui/Header";
+
+export default function InputDOPage() {
+  return (
+    <div className="space-y-6">
+      <Header title="Input DO" subtitle="Input Delivery Order" />
+    </div>
+  );
+}
